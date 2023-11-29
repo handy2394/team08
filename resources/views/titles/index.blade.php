8544133df0858be9c6ee2dfe93ba9a6194ee9b8a
d1104181708 <d1104181708@gm.lhu.edu.tw>
2023-11-29 09:59:23 +0800

    1. 以表格方式呈現 titles.index / parties.index 視圖(view); 2. 增加show、edit的路由及簡易的parties/titles控制器方法(show, edit)

--- resources/views/titles/index.blade.php
+++ resources/views/titles/index.blade.php
@@ -5,18 +5,45 @@
 
 <body>
 <h1>列出所有議員</h1>
-@for($i=0; $i<count($titles);$i++)
-    {{$titles[$i]['name'] }} <br/>
-    {{$titles[$i]['gender'] }} <br/>
-    {{$titles[$i]['session'] }} <br/>
-    {{$titles[$i]['title'] }} <br/>
-    {{$titles[$i]['city'] }} <br/>
-    {{$titles[$i]['birthday'] }} <br/>
-    {{$titles[$i]['area'] }} <br/>
-    {{$titles[$i]['cellphone'] }} <br/>
-    {{$titles[$i]['address'] }} <br/>
-    {{$titles[$i]['website'] }} <br/>
-    {{$titles[$i]['tid'] }} <br/><br/>
-@endfor    
+
+<table>
+    <tr>
+        <th>編號</th>
+        <th>姓名</th>
+        <th>性別</th>
+        <th>屆</th>
+        <th>民意代表</th>
+        <th>縣市</th>
+        <th>生日</th>
+        <th>選區</th>
+        <th>電話</th>
+        <th>通訊處</th>
+        <th>個人網站</th>
+        <th>黨派</th>
+        <th>操作1</th>
+        <th>操作2</th>
+        <th>操作3</th>
+    </tr>
+    @for($i=0; $i<count($titles);$i++)
+    <tr>
+        <td>{{$titles[$i]['id'] }}</td>
+        <td>{{$titles[$i]['name'] }}</td>
+        <td>{{$titles[$i]['gender'] }}</td>
+        <td>{{$titles[$i]['session'] }}</td>
+        <td>{{$titles[$i]['title'] }}</td>
+        <td>{{$titles[$i]['city'] }}</td>
+        <td>{{$titles[$i]['birthday'] }}</td>
+        <td>{{$titles[$i]['area'] }}</td>
+        <td>{{$titles[$i]['cellphone'] }}</td>
+        <td>{{$titles[$i]['address'] }}</td>
+        <td>{{$titles[$i]['website'] }}</td>
+        <td>{{$titles[$i]['tid'] }}</td>
+        <td><a href="{{ route('titles.show', ['id'=>$titles[$i]['id']]) }}">顯示</a></td>
+        <td><a href="{{ route('titles.edit', ['id'=>$titles[$i]['id']]) }}">修改</a></td>    
+        <td>刪除</td>   
+    </tr>
+
+    @endfor  
+</table>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,27 @@ use App\Http\Controllers\TitlesController;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('parties',[partiesController::class,'index'])->name('parties.index');
 
-Route::get('titles',[TitlesController::class,'index'])->name('titles.index');
+Route::get('titles',[TitlesController::class,'index'])->name('titles.index');*/
+Route::get('/', function () {
+    return redirect('titles');
+});
+
+Route::get('titles', [TitlesController::class, 'index'])->name('titles.index');
+// 顯示
+Route::get('titles/{id}', [TitlesController::class, 'show'])->where('id', '[0-9]+')->name('titles.show');
+// 修改
+Route::get('titles/{id}/edit', [TitlesController::class, 'edit'])->where('id', '[0-9]+')->name('titles.edit');
+
+Route::get('parties', [PartiesController::class, 'index'])->name('parties.index');
+// 顯示
+Route::get('parties/{id}', [PartiesController::class, 'show'])->where('id', '[0-9]+')->name('parties.show');
+// 修改
+Route::get('parties/{id}/edit', [PartiesController::class, 'edit'])->where('id', '[0-9]+')->name('parties.edit');
+
 
