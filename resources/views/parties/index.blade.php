811fe47db5191ffc8e16735e94edc1b1816e7d2e
F308\F308 <d1104181012@gm.lhu.edu.tw>
2023-11-29 12:05:49 +0800

    新增板模配置並修改titles.index and parties.index等視圖

--- resources/views/parties/index.blade.php
+++ resources/views/parties/index.blade.php
@@ -1,18 +1,35 @@
-<html>
-<head>
-    <title>政黨名單</title>
-</head>
 
+@extends('app')
 
-<body>
-@for($i=0;$i<count($parties);$i++)
-{{$parties[$i]['id'] }} <br/>
-{{$parties[$i]['tname'] }} <br/>
-{{$parties[$i]['chairman'] }}<br/>
-{{$parties[$i]['secretary'] }}<br/>
-{{$parties[$i]['standpoint'] }}<br/>
-{{$parties[$i]['link'] }}<br/><br/>
-@endfor
-</body>
+@section('title', '列出所有議員')
 
-</html>
+@section('Ttitles')
+<h1>列出所有政黨</h1>
+
+<table>
+    <tr>
+        <th>編號</th>
+        <th>黨名</th>
+        <th>黨主席</th>
+        <th>秘書長</th>
+        <th>政治立場</th>
+        <th>官方網站</th>
+        <th>操作1</th>
+        <th>操作2</th>
+        <th>操作3</th>
+    </tr>
+    @for($i=0; $i<count($parties); $i++)
+        <tr>
+            <td>{{ $players[$i]['id'] }}</td>
+            <td>{{ $players[$i]['tname'] }}</td>
+            <td>{{ $players[$i]['chairman'] }}</td>
+            <td>{{ $players[$i]['secretary'] }}</td>
+            <td>{{ $players[$i]['standpoint'] }}</td>
+            <td>{{ $players[$i]['link'] }}</td>
+            <td><a href="{{ route('parties.show', ['id'=>$parties[$i]['id']]) }}">顯示</a></td>
+            <td><a href="{{ route('parties.edit', ['id'=>$parties[$i]['id']]) }}">修改</a></td>    
+            <td>刪除</td>    
+        </tr>
+    @endfor
+<table>
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,22 @@ use App\Http\Controllers\TitlesController;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('parties',[partiesController::class,'index'])->name('parties.index');
+
+// Route::get('titles',[TitlesController::class,'index'])->name('titles.index');
+
+// 註解一下
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('titles');
 });
+Route::get('titles', [PlayersController::class, 'index'])->name('titles.index');
+Route::get('titles/{id}', [PlayersController::class, 'show'])->where('id', '[0-9]+')->name('titles.show');
+Route::get('titles/{id}/edit', [PlayersController::class, 'edit'])->where('id', '[0-9]+')->name('titles.edit');
 
-Route::get('parties',[partiesController::class,'index'])->name('parties.index');
-
-Route::get('titles',[TitlesController::class,'index'])->name('titles.index');
-
+Route::get('parties', [TeamsController::class, 'index'])->name('parties.index');
+Route::get('parties/{id}', [TeamsController::class, 'show'])->where('id', '[0-9]+')->name('parties.show');
+Route::get('parties/{id}/edit', [TeamsController::class, 'edit'])->where('id', '[0-9]+')->name('parties.edit');
