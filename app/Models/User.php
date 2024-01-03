15b7364228d864846152f288a1f5566fcf74dcd2
BaGaJum <d1104181032@gm.lhu.edu.tw>
2024-01-03 11:54:41 +0800

    1. 編輯 app/User.php，加入三種帳號角色名稱的常數字串，並將 role 這個欄位加入 fillable 中。 2. 編輯 database/migrations/2014_10_12_000000_create_users_table.php 這一個建立使用者資料表的 migration 設定檔，在其中加入一欄用於儲存帳號角色的 role 欄位。 3. 編輯 app/Http/Controllers/Auth/RegisterController.php，設定使用者註冊時的角色都是一般使用者。 4. 建立 Gate 權限規則。

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,10 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    
+    const ROLE_ADMIN="admin";
+    const ROLE_MANAGER='manager';
+    const ROLE_USER="user"; 
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +25,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
+
     ];
 
     /**
